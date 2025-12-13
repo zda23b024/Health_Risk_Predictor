e401719cbbd9c18d70a134c7ed26405a182f5c55
Anwar-Aziz <zda23b020@iitmz.ac.in>
2025-12-13 22:50:01 +0300

    added App.py to frontend

--- backend/App.py
+++ backend/App.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from database import init_db
 from routes.health_routes import health_bp
+from routes.stats_routes import stats_bp
 
 
 def create_app():
@@ -10,8 +11,12 @@ def create_app():
     # Enable CORS for all origins (simplest for local dev)
     CORS(app)
 
+    # Initialize DB tables
+    init_db()
+
     # Register blueprints
     app.register_blueprint(health_bp)
+    app.register_blueprint(stats_bp)
 
     return app
 
@@ -20,7 +25,4 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    # Create tables if not exist
-    init_db()
-    # Run dev server
     app.run(host="0.0.0.0", port=5000, debug=True)
